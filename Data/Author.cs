2f36919c991c4d4378d060f575bd48b32c34e3da
Gustave TSOPMO <gustavetsopmo.gt@gmail.com>
2023-01-17 01:49:15 +0100

    Add Ef Core and Scaffold the Database

---
+++ Data/Author.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookStore.Api.Data;
+
+public partial class Author
+{
+    public int Id { get; set; }
+
+    public string? FistName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public string? Bio { get; set; }
+
+    public virtual ICollection<Book> Books { get; } = new List<Book>();
+}
